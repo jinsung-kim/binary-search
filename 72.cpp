71133e628a02e43e1778ff2f3c4e643aa4c5a66d
Jin Kim <jinsungkim1210@gmail.com>
2022-01-11 08:38:00 -0800

    .

---
+++ 72.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+
+class Tree
+{
+public:
+    int val;
+    Tree *left;
+    Tree *right;
+};
+
+Tree *solve(Tree *root)
+{
+    stack<Tree *> st;
+    st.push(root);
+
+    while (!st.empty())
+    {
+        Tree *top = st.top();
+        st.pop();
+        if (top)
+        {
+            st.push(top->left);
+            st.push(top->right);
+            swap(top->left, top->right);
+        }
+    }
+
+    return root;
+}
